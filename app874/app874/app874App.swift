21e9cf9d19c45f8ee8ccc61d464e5b882d936370
Dias <dias.atudinov@gmail.com>
2024-09-03 18:36:45 +0500

    tab1 part3

---
+++ app874/app874/app874App.swift
@@ -0,0 +1,17 @@
+//
+//  app874App.swift
+//  app874
+//
+//  Created by Dias Atudinov on 03.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct app874App: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeUIView(viewModel: HomeViewModel())
+        }
+    }
+}
