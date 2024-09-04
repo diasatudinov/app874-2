3d654498c5eb3eafcb7be420b9f1ab3e1d18683f
Dias <dias.atudinov@gmail.com>
2024-09-04 20:38:34 +0500

    new files for tab 3 and settings view

---
+++ app874/app874/First/Procedures/View/ProceduresUIView.swift
@@ -0,0 +1,18 @@
+//
+//  ProceduresUIView.swift
+//  app874
+//
+//  Created by Dias Atudinov on 04.09.2024.
+//
+
+import SwiftUI
+
+struct ProceduresUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProceduresUIView()
+}
